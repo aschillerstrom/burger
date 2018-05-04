186ba8f5e2036bc4287a18457e32a4796d39d1f5
Amy Schillerstrom <amyschillerstrom@amys-mbp.attlocal.net>
2018-05-04 18:46:12 -0500

    router

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 
 //app.use(express, static('public'));
-app.use(express.static(process.cwd() + '/public'));
+app.use(express.static('/public'));
 
 // sets up the handlebars
 var exphbs = require("express-handlebars");
